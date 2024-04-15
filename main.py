da9238af8d12fb4a2418c69f92db456139815c2a
dwahl-21 <131616808+dwahl-21@users.noreply.github.com>
2024-04-15 19:24:11 +0200

    added CLI loop to search for tags and output results

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import re
 import os
 import hashlib
 
-DATAROOT = os.path.abspath(os.path.expanduser("~/Diginoz/root"))
+DATAROOT = os.path.abspath(os.path.expanduser("~/diginoz/root"))
 
 def getIndex(root: str) -> dict:
     '''
@@ -113,7 +113,7 @@ def searchTag(conn: sql.Connection, tag: str) -> tuple[dict]|None:
         result (tuple): a list of entries which match the tag
     '''
     cursor: sql.Cursor = conn.cursor()
-    result: tuple[dict]|None = cursor.execute(f"SELECT * FROM files WHERE tags LIKE '%{tag}%';").fetchall()
+    result: tuple[dict]|None = cursor.execute(f"SELECT pathHash,path,tags FROM files WHERE tags LIKE '%{tag}%';").fetchall()
     cursor.close()
     return result
 
@@ -121,19 +121,29 @@ if __name__ == "__main__":
     with sql.connect("diginoz.db") as conn:
         cursor: sql.Cursor = conn.cursor()
 
-        # list all currently tracked files in the database
-        getFiles(conn)
-        print("-"*64)
+        # input from the user, either a tag or 'QUIT'
+        option: str = ""
+        while option != "QUIT":
+            os.system("cls")
+            # list all currently tracked files in the database
+            # getFiles(conn)
+            # print("-"*64)
 
-        # remove old hashes before updating and inserting new ones
-        # to circumvent moving files and having duplicate entries
-        removeOldHashes(conn)
+            # remove old hashes before updating and inserting new ones
+            # to circumvent moving files and having duplicate entries
+            removeOldHashes(conn)
 
-        # build index for data root
-        index: list = getIndex(DATAROOT)
-        for file in index:
-            updateHashes(conn, file)
+            # build index for data root
+            index: list = getIndex(DATAROOT)
+            for file in index:
+                updateHashes(conn, file)
 
-        # search for a specific tag and print out the result
-        print(searchTag(conn, "diginoz"))
+            # search for a specific tag and print out the result
+            results: tuple[dict] = searchTag(conn, option.lower())
+
+            for result in results:
+                path: str = result[1].replace("\\","/")
+                print(f"file://{path} <{result[2]}>")
+                print("")
+            option = input("Tag to search for (QUIT to exit program): ")
         cursor.close()
